04f43d60461a0596332f63086fe3c51fde0c417a
Христофорова Полина Андреевна <hristoforova@miit.ru>
2023-03-24 15:10:42 +0300

    [part4/bonus] completed ex02

--- part4.sql
+++ part4.sql
@@ -23,7 +23,7 @@
  * 1)
  * Drop all those tables whose names begin with the phrase 'TableName'
  */
-create or replace procedure drop_tables_started_TableName() as
+create or replace procedure prcdr_drop_tables_started_TableName() as
 $$
 declare
 	rec record;
@@ -42,4 +42,36 @@ end;
 $$ language plpgsql;
 
 -- CALL PROCEDURE --
--- call drop_tables_started_TableName();
+-- call prcdr_drop_tables_started_TableName();
+
+
+/*
+ * 2)
+ * Output a list of functions' names with their parameters
+ * and number of found funcs (just functions which have params)
+ */
+create or replace procedure prcdr_funcs_with_arguments(
+	funcs inout text,
+	numb inout int
+) as
+$$
+declare
+	line record;
+begin
+	for line in select (
+				p.proname || ' (' || pg_get_function_arguments(p.oid) || ')'
+			) as functions_list
+		from pg_catalog.pg_namespace n
+			join pg_catalog.pg_proc p on p.pronamespace = n.oid
+		where p.prokind = 'f'
+			and n.nspname = 'public'
+			and (pg_get_function_arguments(p.oid) = '') is not true
+		loop
+		funcs := (funcs || line.functions_list || E'\n');
+		numb := numb + 1;
+	end loop;
+end;
+$$ language plpgsql;
+
+-- CALL PROCEDURE --
+-- call prcdr_funcs_with_arguments('', 0);
